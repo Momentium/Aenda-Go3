9aa6daa659245647783cdcd9787a8b1f88a20e39
SinJ <johankim27@gmail.com>
2021-03-02 18:36:42 +0900

    0302 | Sinbad | update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { StylesProvider } from '@material-ui/core/styles';
-import GlobalStyle from './styles/globalStyles';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StylesProvider } from "@material-ui/core/styles";
+import GlobalStyle from "./styles/globalStyles";
+
+document.documentElement.addEventListener(
+  "touchstart",
+  function (event) {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  },
+  false
+);
+let lastTouchEnd = 0;
+document.documentElement.addEventListener(
+  "touchend",
+  function (event) {
+    let now = new Date().getTime();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  },
+  false
+);
 
 ReactDOM.render(
   <StylesProvider injectFirst>
     <React.StrictMode>
-      <GlobalStyle/>
+      <GlobalStyle />
       <App />
     </React.StrictMode>
   </StylesProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
