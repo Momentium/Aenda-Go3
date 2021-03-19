683eb315ada10594368090d8861fbf0f0ddbd3fd
SinJ <johankim27@gmail.com>
2021-03-19 15:08:19 +0900

    0319 | Sinbad | update

--- src/components/pages/main/slide/Slide.js
+++ src/components/pages/main/slide/Slide.js
@@ -1,82 +1,83 @@
-import React, { useEffect, useRef } from "react";
-import styled from "styled-components";
+import React, { useContext, useEffect, useRef } from "react";
+import styled, { ThemeContext } from "styled-components";
 import Hashtag from "./Hashtag";
 import { hashTagData } from "../../../../data/data";
 
+
 const Slide = ({ dir, dataIdx, pauseIdx, setPauseIdx }) => {
   const contRef = useRef(undefined);
-
+  const stateRef = useRef('run');
+  
   useEffect(() => {
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                                 window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
 
     const _div = contRef.current;
-    let raf;
 
+    let raf;
     const _1st = _div.childNodes[0];
     const _2nd = _div.childNodes[1];
     _1st.style[dir] = `0px`;
-    _1st.style.zIndex = '1';
     _2nd.style[dir] = `${_2nd.clientWidth}px`;
-    _2nd.style.zIndex = '1';
 
     const run = () => {
       let _1stX = Number(_1st.style[dir].replace('px', ''));
       let _2ndX = Number(_2nd.style[dir].replace('px', ''));
 
-      let _state = sessionStorage.getItem(`${dataIdx} state`);
       let _px;
-      switch(_state) {
-        case 'run':
-          window.innerWidth > 480 ? _px = 3 : _px = 1 ;
-          
-          break;
-        case 'slow':
+
+      if(stateRef.current === 'run') {
+        if(window.innerWidth > 480) {
+          _px = 2;
+        }
+        else {
           _px = 1;
-          break;
-        case 'stop':
-          _px = 0;
-          break;
-        default: break;
+        }
+      }
+      else if(stateRef.current === 'slow') {
+        _px = 1;
+      }
+      else if(stateRef.current === 'stop') {
+        _px = 0;
       }
   
-      if(_1stX < -1 * _1st.clientWidth) {
-        _1st.style[dir] = `${_1st.clientWidth}px`;
+      if(_1stX <= -1 * _1st.clientWidth) {
+        _1st.style[dir] = `${-1 * _px}px`;
+        _2nd.style[dir] = `${_2nd.clientWidth - _px}px`;
       }
       else {
         _1st.style[dir] = `${_1stX - _px}px`;
-      }
-      if(_2ndX < -1 * _2nd.clientWidth) {
-        _2nd.style[dir] = `${_2nd.clientWidth}px`;
-      }
-      else {
         _2nd.style[dir] = `${_2ndX - _px}px`;
       }
       raf = requestAnimationFrame(run);
     }
-    raf = requestAnimationFrame(run);
-    return () => {
-      cancelAnimationFrame(raf)
-    }; 
-  }, [dir, dataIdx]);
+    setTimeout(() => { 
+      run();
+    }, 1000);
 
+    return () => {
+      cancelAnimationFrame(raf);
+    };
+  }, []);
 
   useEffect(() => {
     if (dataIdx === pauseIdx) {
-      sessionStorage.setItem(`${dataIdx} state`, 'stop')
+      stateRef.current = 'stop';
     } else {
-      sessionStorage.setItem(`${dataIdx} state`, 'run')
+      stateRef.current = 'run';
     }
   }, [pauseIdx, dataIdx]);
 
   const slowSlide = () => {
     if (dataIdx === pauseIdx) return;
-    sessionStorage.setItem(`${dataIdx} state`, 'slow')
+    if (window.innerWidth <= 480) return;
+    stateRef.current = 'slow';
   };
   const runSlide = () => {
     if (dataIdx === pauseIdx) return;
-    sessionStorage.setItem(`${dataIdx} state`, 'run')
+    if (window.innerWidth <= 480) return;
+    stateRef.current = 'run';
   };
 
   const hashTagList = hashTagData[dataIdx].map((el, idx) => {
@@ -126,43 +127,9 @@ const StSlideCont = styled.div`
     margin: ${({ theme }) => theme.calcVW_M(8)} 0;
   }
 
-  @keyframes slide-all-left {
-    0% {
-      transform: translateX(0%);
-    }
-    100% {
-      transform: translateX(-3000%);
-    }
-  }
-
-  @keyframes slide-all-right {
-    0% {
-      transform: translateX(0%);
-    }
-    100% {
-      transform: translateX(3000%);
-    }
-  }
 `;
 
 const StHTCont = styled.span`
   position: absolute;
   white-space: nowrap;
-
-  @keyframes slide-left {
-    0% {
-      transform: translateX(0%);
-    }
-    100% {
-      transform: translateX(-100%);
-    }
-  }
-  @keyframes slide-right {
-    0% {
-      transform: translateX(0%);
-    }
-    100% {
-      transform: translateX(100%);
-    }
-  }
 `;
